a23bdea5bd02b309e61dcab26faff1bbefeb7291
Carlos Duran <carlosduran.xyz@gmail.com>
2020-10-29 04:44:28 -0500

    Use env PORT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", async () => ({ hello: "world" }));
 
 const start = async () => {
   try {
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
     app.log.info(`server listening on ${app.server.address().port}`);
   } catch (err) {
     app.log.error(err);
